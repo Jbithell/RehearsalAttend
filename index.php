394893400604f9ce02e7eaddc71d0a3abbca0a87
James Bithell <hi@jbithell.com>
2016-12-14 18:42:23 +0000

    Tidy up

--- index.php
+++ index.php
@@ -6,12 +6,13 @@ require_once __DIR__ . "/config.php";
  * Date: 14/12/2016
  * Time: 04:15 PM
  */
-echo "Test";
-$email = new BluePost\Email(
+/*
+    $email = new BluePost\Email(
     new BluePost\Person("Rehearsal Attendance Manager", "donotreply@rehersalattend.online"),
     [new BluePost\Person("James TESTER", "james.bithell@westminster.org.uk")], [], [],
     "This is sent using BlueEmail"
-);
+    );
 
-$email->setBody("emails/notification.twig");
-$email->send();
+    $email->setBody("emails/notification.twig");
+    //$email->send();
+ */
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . "/config.php";
+/**
+ * Created by PhpStorm.
+ * User: james
+ * Date: 14/12/2016
+ * Time: 04:15 PM
+ */
